62cedd9aef7989c31721471146943098a3d4f346
Gregg Keary <gregg@gkeary.com>
2010-06-02 06:22:50 -0400

    initial commit

---
+++ WCFConsole/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace WCFConsole
+{
+
+    //    http://msdn.microsoft.com/en-us/library/bb412178%28v=VS.90%29.aspx
+    //    paste the interface, the class and main() into the program.cs file...
+    //    Mung it around so it does the following:
+    //      a. calls putty with a username/pwd and file to execute.
+    //      b. returns the stdoutput
+    //
+    //      c. see the resuts in the console
+    //      d. see the results in a browser.
+    //
+    //      then mung it again so it 
+    //          a. returns a list of IP1, name1, macaddrss mth modeltype mh modelname 'devicename'
+    //          b. updates notes for a given ip...
+    //
+    //       implement soap, cache, user roles etc..  later on
+    //       
+    //       use xml and/or json for messages
+   //  cf  plink.exe in the putty directory...
+   //    link is: http://e-articles.info/e/a/title/Using-Plink-to-initiate-an-SSH-session-from-the-command-line-or-a-script/
+   //
+
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet]
+        string EchoWithGet(string s);
+
+        [OperationContract]
+        [WebInvoke]
+        string EchoWithPost(string s);
+    }
+    public class Service : IService
+    {
+        public string EchoWithGet(string s)
+        {
+            return "You said " + s;
+        }
+
+        public string EchoWithPost(string s)
+        {
+            return "You said " + s;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServiceHost host = new WebServiceHost(typeof(Service), new Uri("http://localhost:8000/"));
+            try
+            {
+                ServiceEndpoint ep = host.AddServiceEndpoint(typeof(IService), new WebHttpBinding(), "");
+                host.Open();
+                using (ChannelFactory<IService> cf = new ChannelFactory<IService>(new WebHttpBinding(), "http://localhost:8000"))
+                {
+                    cf.Endpoint.Behaviors.Add(new WebHttpBehavior());
+
+                    IService channel = cf.CreateChannel();
+
+                    string s;
+
+                    Console.WriteLine("Calling EchoWithGet via HTTP GET: ");
+                    s = channel.EchoWithGet("Hello, world");
+                    Console.WriteLine("   Output: {0}", s);
+
+                    Console.WriteLine("");
+                    Console.WriteLine("This can also be accomplished by navigating to");
+                    Console.WriteLine("http://localhost:8000/EchoWithGet?s=Hello, world!");
+                    Console.WriteLine("in a web browser while this sample is running.");
+
+                    Console.WriteLine("");
+
+                    Console.WriteLine("Calling EchoWithPost via HTTP POST: ");
+                    s = channel.EchoWithPost("Hello, world");
+                    Console.WriteLine("   Output: {0}", s);
+                    Console.WriteLine("");
+                }
+
+                Console.WriteLine("Press <ENTER> to terminate");
+                Console.ReadLine();
+
+                host.Close();
+            }
+            catch (CommunicationException cex)
+            {
+                Console.WriteLine("An exception occurred: {0}", cex.Message);
+                host.Abort();
+            }
+        }
+    }
+}
